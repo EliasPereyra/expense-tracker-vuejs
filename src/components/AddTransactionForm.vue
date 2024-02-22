1c837f54b6f87e50f9509637395df376925ca4cd
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-02-21 22:52:24 -0300

    Create a func for creating a new transaction

--- src/components/AddTransactionForm.vue
+++ src/components/AddTransactionForm.vue
@@ -1,30 +1,51 @@
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+
+const emit = defineEmits(["transactionSubmit"]);
+const text = ref("");
+const amount = ref("");
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    alert("You have to enter the values");
+  }
+  const transaction = {
+    text: text.value,
+    amount: amount.value,
+  };
+
+  emit("transactionSubmit", transaction);
+
+  text.value = "";
+  amount.value = "";
+};
+</script>
+
 <template>
-  <section>
+  <form @submit.prevent="onSubmit">
     <h4>Create a new transaction</h4>
 
     <div id="text-wrapper">
       <label> Text </label>
-      <input type="text" placeholder="Computer, Salary, Camera... " />
-    </div>
-
-    <div>
-      <select>
-        <option value="Income">Income</option>
-        <option value="Expense">Expense</option>
-      </select>
+      <input
+        type="text"
+        id="text"
+        v-model="text"
+        placeholder="Computer, Salary, Camera... "
+      />
     </div>
 
     <div id="amount-wrapper">
       <label> Amount </label>
-      <input type="number" placeholder="" />
+      <input type="number" id="amount" v-model="amount" placeholder="" />
     </div>
 
     <button>Create transaction</button>
-  </section>
+  </form>
 </template>
 
 <style scoped>
-section {
+form {
   h4 {
     margin: 1.2rem 0;
   }
